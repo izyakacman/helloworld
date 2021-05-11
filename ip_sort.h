5f24c42a28172f642d796db831b8cfff4e8c4ae5
aliensol <aliensol@mail.ru>
2021-05-11 19:03:36 +0300

    revert all files

---
+++ ip_sort.h
@@ -0,0 +1,13 @@
+#ifndef IP_SORT
+#define IP_SORT
+
+#include <vector>
+
+using namespace std;
+
+/**
+	Reverse lexicographically sort
+*/
+void IpSort(vector<vector<int>>& ipPool);
+
+#endif // !IP_SORT
